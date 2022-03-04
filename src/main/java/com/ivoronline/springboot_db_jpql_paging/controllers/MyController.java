937f684aa97e1d7e3e49a99d01776a58063f4440
igoreta <ivoronline@gmail.com>
2022-03-04 11:30:23 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_db_jpql_paging/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_db_jpql_paging/controllers/MyController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class MyController {
   @Autowired PersonRepository personRepository;
 
   //================================================================
-  // GET PERSONS BY AGE
+  // GET PERSONS
   //================================================================
   // http://localhost:8080/GetPersonsByAge?pageNumber=0&pageSize=2&age=50
   @RequestMapping("GetPersons")
@@ -33,4 +34,19 @@ public class MyController {
     return personsList;
   }
 
+  //================================================================
+  // GET PERSONS SORTED
+  //================================================================
+  // http://localhost:8080/GetPersonsSorted?pageNumber=0&pageSize=2&age=50
+  @RequestMapping("GetPersonsSorted")
+  List<Person> getPersonsSorted(
+    @RequestParam Integer pageNumber,
+    @RequestParam Integer pageSize,
+    @RequestParam Integer age
+  ) {
+    Pageable     firstPageWithTwoElements = PageRequest.of(pageNumber, pageSize, Sort.by("age").descending().and(Sort.by("name")));
+    Page<Person> personsPage              = personRepository.getPersons(age, firstPageWithTwoElements);
+    List<Person> personsList              = personsPage.getContent();
+    return personsList;
+  }
 }
